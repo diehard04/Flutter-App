c543e238f5405a7d4441d39cc81e63dee26eb097
Dipak <dipakmehta0910@gmail.com>
2020-09-21 20:07:16 +0530

    button

--- lib/fragments/home_fragment.dart
+++ lib/fragments/home_fragment.dart
@@ -3,8 +3,91 @@ import 'package:flutter/material.dart';
 class HomeFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text("Home Fragment"),
+    return new Scaffold(
+        body: Center(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: MaterialButton(
+                          height: 100.0,
+                          minWidth: 150.0,
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                          child: new Text("Profile"),
+                          // onPressed: () => {
+                          // Navigator.push(
+                          // context,
+                          // MaterialPageRoute(builder: (context) => Profile()),
+                          // )
+                          // },
+                          splashColor: Colors.redAccent,
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: new MaterialButton(
+                          height: 100.0,
+                          minWidth: 150.0,
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                          child: new Text("Menu"),
+                          // onPressed: () => {
+                          // Navigator.push(
+                          // context,
+                          // MaterialPageRoute(builder: (context) => Menu()),
+                          // )
+                          // },
+                          splashColor: Colors.redAccent,
+                        )
+                    ),
+                  ],
+                ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: <Widget>[
+                //     Padding(
+                //         padding: EdgeInsets.all(20.0),
+                //         child: new MaterialButton(
+                //           height: 100.0,
+                //           minWidth: 150.0,
+                //           color: Theme.of(context).primaryColorDark,
+                //           textColor: Colors.white,
+                //           child: new Text("Settings"),
+                //           // onPressed: () => {
+                //           // Navigator.push(
+                //           // context,
+                //           // MaterialPageRoute(builder: (context) => Settings()),
+                //           // )
+                //           // },
+                //           splashColor: Colors.redAccent,
+                //         )),
+                //     Padding(
+                //         padding: EdgeInsets.all(20.0),
+                //         child: new MaterialButton(
+                //           height: 100.0,
+                //           minWidth: 150.0,
+                //           color: Theme.of(context).primaryColor,
+                //           textColor: Colors.white,
+                //           child: new Text("About"),
+                //           // onPressed: () => {
+                //           // Navigator.push(
+                //           // context,
+                //           // MaterialPageRoute(builder: (context) => About()),
+                //           // )
+                //           // },
+                //           splashColor: Colors.redAccent,
+                //         )),
+                //   ],
+                // ),
+      
+              ],
+            )
+
+        ),
     );
   }
 }
