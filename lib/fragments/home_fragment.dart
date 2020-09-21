08c541f2b28756698431b80c0b61491af9a80b23
DIPAK KUMAR MEHTA <dipakmehta0910@gmail.com>
2020-09-21 21:32:16 +0530

    navigaion buttin

--- lib/fragments/home_fragment.dart
+++ lib/fragments/home_fragment.dart
@@ -12,21 +12,53 @@ class HomeFragment extends StatelessWidget {
                   children: <Widget>[
                     Padding(
                         padding: EdgeInsets.all(20.0),
-                        child: MaterialButton(
+                        child: RaisedButton(
+                          child: Text('Color Changed'),
+                          color: Colors.green,
+                          onPressed: () {},
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Material(
+                           shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(22.0) ),
+                            elevation: 18.0,
+                            color: Color(0xFF801E48),
+                            clipBehavior: Clip.antiAlias, // Add This
+                            child: MaterialButton(
+                            minWidth: 100.0,
+                            height: 35,
+                            color: Color(0xFF801E48),
+                            child: new Text('Preciso de ajuda',
+                            style: new TextStyle(fontSize: 16.0, color: Colors.white)),
+                            onPressed: () {
+
+                              },
+                            ),
+
+                        )
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: new MaterialButton(
                           height: 100.0,
                           minWidth: 150.0,
-                          color: Theme.of(context).primaryColor,
+                          color: Theme.of(context).primaryColorDark,
                           textColor: Colors.white,
-                          child: new Text("Profile"),
+                          child: new Text("Settings"),
                           // onPressed: () => {
                           // Navigator.push(
                           // context,
-                          // MaterialPageRoute(builder: (context) => Profile()),
+                          // MaterialPageRoute(builder: (context) => Settings()),
                           // )
                           // },
                           splashColor: Colors.redAccent,
-                        )
-                    ),
+                        )),
                     Padding(
                         padding: EdgeInsets.all(20.0),
                         child: new MaterialButton(
@@ -34,55 +66,17 @@ class HomeFragment extends StatelessWidget {
                           minWidth: 150.0,
                           color: Theme.of(context).primaryColor,
                           textColor: Colors.white,
-                          child: new Text("Menu"),
+                          child: new Text("About"),
                           // onPressed: () => {
                           // Navigator.push(
                           // context,
-                          // MaterialPageRoute(builder: (context) => Menu()),
+                          // MaterialPageRoute(builder: (context) => About()),
                           // )
                           // },
                           splashColor: Colors.redAccent,
-                        )
-                    ),
+                        )),
                   ],
                 ),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.center,
-                //   children: <Widget>[
-                //     Padding(
-                //         padding: EdgeInsets.all(20.0),
-                //         child: new MaterialButton(
-                //           height: 100.0,
-                //           minWidth: 150.0,
-                //           color: Theme.of(context).primaryColorDark,
-                //           textColor: Colors.white,
-                //           child: new Text("Settings"),
-                //           // onPressed: () => {
-                //           // Navigator.push(
-                //           // context,
-                //           // MaterialPageRoute(builder: (context) => Settings()),
-                //           // )
-                //           // },
-                //           splashColor: Colors.redAccent,
-                //         )),
-                //     Padding(
-                //         padding: EdgeInsets.all(20.0),
-                //         child: new MaterialButton(
-                //           height: 100.0,
-                //           minWidth: 150.0,
-                //           color: Theme.of(context).primaryColor,
-                //           textColor: Colors.white,
-                //           child: new Text("About"),
-                //           // onPressed: () => {
-                //           // Navigator.push(
-                //           // context,
-                //           // MaterialPageRoute(builder: (context) => About()),
-                //           // )
-                //           // },
-                //           splashColor: Colors.redAccent,
-                //         )),
-                //   ],
-                // ),
       
               ],
             )
