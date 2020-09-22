f68150ee66cef406a348d73f7ab6a0c64235562b
Dipak <dipakmehta0910@gmail.com>
2020-09-22 17:38:06 +0530

    dash board

--- lib/fragments/home_fragment.dart
+++ lib/fragments/home_fragment.dart
@@ -1,87 +1,93 @@
 import 'package:flutter/material.dart';
 
 class HomeFragment extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        body: Center(
-            child: Column(
-              children: <Widget>[
+    
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
+
+          child: Column(
+
+            children: [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  
                   children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: RaisedButton(
-                          child: Text('Color Changed'),
-                          color: Colors.green,
-                          onPressed: () {},
-                        )
+                    new Container(
+                        margin: const EdgeInsets.only(left :10.0),
+                        child:  Text( "Last Refreshed: "),
                     ),
-                    Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Material(
-                           shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(22.0) ),
-                            elevation: 18.0,
-                            color: Color(0xFF801E48),
-                            clipBehavior: Clip.antiAlias, // Add This
-                            child: MaterialButton(
-                            minWidth: 100.0,
-                            height: 35,
-                            color: Color(0xFF801E48),
-                            child: new Text('Preciso de ajuda',
-                            style: new TextStyle(fontSize: 16.0, color: Colors.white)),
-                            onPressed: () {
-
-                              },
-                            ),
-
-                        )
-                    ),
+                      new Container(
+                        margin: const EdgeInsets.only(left :10.0),
+                        child:  Text("22 - 09 2020"),
+                    ), 
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: new MaterialButton(
-                          height: 100.0,
-                          minWidth: 150.0,
-                          color: Theme.of(context).primaryColorDark,
-                          textColor: Colors.white,
-                          child: new Text("Settings"),
-                          // onPressed: () => {
-                          // Navigator.push(
-                          // context,
-                          // MaterialPageRoute(builder: (context) => Settings()),
-                          // )
-                          // },
-                          splashColor: Colors.redAccent,
-                        )),
-                    Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: new MaterialButton(
-                          height: 100.0,
-                          minWidth: 150.0,
-                          color: Theme.of(context).primaryColor,
-                          textColor: Colors.white,
-                          child: new Text("About"),
-                          // onPressed: () => {
-                          // Navigator.push(
-                          // context,
-                          // MaterialPageRoute(builder: (context) => About()),
-                          // )
-                          // },
-                          splashColor: Colors.redAccent,
-                        )),
-                  ],
-                ),
-      
-              ],
-            )
 
+                Expanded(child:
+                    GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 12.0,
+                    mainAxisSpacing: 12.0,
+                    padding: EdgeInsets.all(3.0),
+                    children: <Widget> [
+                      makeDashboardItem("Emergency", Icons.alarm, Colors.red),
+                      makeDashboardItem("Critical", Icons.alarm, Colors.redAccent[200]),
+                      makeDashboardItem("Major", Icons.alarm, Colors.yellow[700]),
+                      makeDashboardItem("Minor", Icons.alarm, Colors.yellow),
+                      makeDashboardItem("Planned", Icons.alarm, Colors.lightBlue[400]),
+                      makeDashboardItem("Normal", Icons.alarm, Colors.green[400])
+
+                    ],
+                  ),
+                )
+            ],
+          ),
+        
         ),
     );
+  }
+
+  Material makeDashboardItem(String title, IconData icon, var c) {
+    return Material(
+        elevation: 14.0,
+        shadowColor: Colors.amber,
+        borderRadius: BorderRadius.circular(24.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: c,
+            borderRadius: BorderRadius.circular(24.0)
+            ),
+          padding: const EdgeInsets.all(10.0),
+          child: new InkWell(
+            onTap: () {
+                print("title " + title);
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              verticalDirection: VerticalDirection.down,
+
+              children: <Widget>[
+                SizedBox(height: 50.0),
+                Center(
+                  child: Icon(
+                  icon,
+                  size: 40.0,
+                  color: Colors.black,
+                )),
+                SizedBox(height: 20.0),
+                new Center(
+                  child: new Text(title,
+                      style: new TextStyle(fontSize: 18.0, color: Colors.black)),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
