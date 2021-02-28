3bbf29291118c8c58b1a69db76fc0b514bd18ae8
DIPAK KUMAR MEHTA <dipakmehta0910@gmail.com>
2021-02-28 23:20:43 +0530

    hori

--- lib/cookbook/GridList.dart
+++ lib/cookbook/GridList.dart
@@ -4,27 +4,51 @@ class GridList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //throw UnimplementedError();
-    final mTitle = '';
+    return HorizonatalList();
+
+    final mTitle = 'verticle list';
+    MaterialApp(
+        title: mTitle,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(mTitle),
+          ),
+          body: GridView.count(
+            crossAxisCount: 3,
+            children: List.generate(100, (index) {
+              return Center(
+                child: Text(
+                  'Item.$index',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              );
+            }),
+          ),
+        ));
+  }
+}
+
+class HorizonatalList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final mTitle = 'Horizontal list';
+
     return MaterialApp(
       title: mTitle,
       home: Scaffold(
         appBar: AppBar(
           title: Text(mTitle),
         ),
-
-        body: GridView.count(
-            crossAxisCount: 3,
-          children: List.generate(100, (index) {
-            return Center(
-              child: Text(
-                'Item.$index',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            );
-          }),
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 30),
+          height: 190.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [],
+          ),
         ),
-      )
+      ),
     );
   }
-
 }
